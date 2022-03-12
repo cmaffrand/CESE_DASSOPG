0166e45747e3d8dda3b3e6959e5c49919b2793f4
Carlos Maffrand <carlosmaffrand5@gmail.com>
2022-03-12 14:01:26 -0300

    Se hacen ejercicios de la segunda clase

--- clase2/ejercicios_clase/personas.py
+++ clase2/ejercicios_clase/personas.py
@@ -15,7 +15,7 @@ class Persona:
         if edad > 0 and edad < 125:
             self.__edad = edad
             
-    def get_nomnbre(self):
+    def get_nombre(self):
         return self.__nombre
     
     def get_edad(self):
@@ -24,7 +24,7 @@ class Persona:
     def print_persona(self):
         print(self)
         print(self.__dict__)
-        print(f"{self.get_nomnbre()} {self.get_edad()}")
+        print(f"{self.get_nombre()} {self.get_edad()}")
         
     def es_mayor_de_edad(self):
         if self.get_edad() >= 18:
@@ -38,13 +38,31 @@ class Persona:
         else:
             return False
         
+    def get_mayor(self, persona):
+        if self.get_edad() >= persona.get_edad():
+            return self.get_nombre()
+        else:
+            return persona.get_nombre()
+        
     @staticmethod
     def dump_csv(name,lista):
         with open(name,"w",encoding="utf-8") as f:
             f.write("Nombre,Edad\n")
             for per in lista:
-                linea = f"{per.get_nomnbre()},{per.get_edad()}\n"
+                linea = f"{per.get_nombre()},{per.get_edad()}\n"
                 f.write(linea)
+                
+    @staticmethod
+    def load_csv(filename):
+        listp = []
+        with open(filename,"r",encoding="utf-8") as f:
+            for l in f:
+                aux = l.strip("\n")
+                aux_split = aux.split(",")
+                if aux_split[1] != "Edad":
+                    p = Persona(aux_split[0],int(aux_split[1]))
+                    listp.append(p)
+            return listp
 
     
 
