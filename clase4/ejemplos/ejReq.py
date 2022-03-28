275e10d626bed8ea66169f090f078ea7e2380ec9
Carlos Maffrand <carlosmaffrand5@gmail.com>
2022-03-28 16:35:34 -0300

    Se agrega material de clase 5 y se modifican ejemplos de clase 4, realizados durante el transcurso de la misma

--- clase4/ejemplos/ejReq.py
+++ clase4/ejemplos/ejReq.py
@@ -3,13 +3,14 @@ import requests
 #https://www.mocky.io/
 
 #URL = "http://localhost:8080/cgi-bin/index.py"
-URL = "http://www.mocky.io/v2/5d797547320000600034ea2b"
-
+#URL = "http://www.mocky.io/v2/5d797547320000600034ea2b"
+URL = "http://www.google.com"
 
 r = requests.get(url = URL) 
 
-if r.status_code==200:
+if r.status_code==200: # 200 es que la respuesta fue correcta
     print(r.text)
+    print(r.status_code)
 else:
     print("error code:"+str(r.status_code))
 
--- clase4/ejemplos/server_python_cgi/cgi-bin/index.py
+++ clase4/ejemplos/server_python_cgi/cgi-bin/index.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+
+# Correr con http://127.0.0.1:8001/cgi-bin/index.py
+
+import cgi
+import cgitb; cgitb.enable()
+
 print("Content-Type: text/html\n")
 print("<!doctype html><title>Hello</title><h2>prueba cgi python</h2>")
+
+arguments = cgi.FieldStorage()
+for i in arguments.keys():
+    print(i + "=" + arguments[i].value)
